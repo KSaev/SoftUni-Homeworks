d2efd9876ada5e79329da2afb12e5a70cb0b3361
KSaev <69797445+KSaev@users.noreply.github.com>
2021-06-30 15:07:58 -0700

    Add files via upload

---
+++ Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-LOVERHL\MSSQLSERVER11;;Database=SoftJail;Trusted_Connection=True";
+    }
+}
---
+++ DataProcessor/ImportDto/OfficerXmlImportModel.cs
@@ -0,0 +1,37 @@
+﻿using SoftJail.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class OfficerXmlImportModel
+    {
+        [XmlElement("Name")]
+        [Required]
+        public string FullName { get; set; }
+
+        [XmlElement("Money")]
+        public decimal Salary { get; set; }
+
+        [XmlElement("Position")]
+        [EnumDataType(typeof(Position))]
+        [Required]
+        public string Position { get; set; }
+        
+
+        [XmlElement("Weapon")]
+        [EnumDataType(typeof(Weapon))]
+        [Required]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public PrisonerXmlImportModel[] Prisoners { get; set; }
+    }
+}
